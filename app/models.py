5a540224f6b6a0124973fec1baccb1e8d3d2c334
Rafa0r7 <rafaelrodriguesrr06@gmail.com>
2025-07-02 19:36:18 -0300

    Add weather data history

--- app/models.py
+++ app/models.py
@@ -13,3 +13,23 @@ class User(db.Model):
     def create(self):
         db.session.add(self)
         db.session.commit()
+
+class Weather(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    city = db.Column(db.String(255),nullable=False)
+    temperature = db.Column(db.Float, nullable=False)
+    feels_like = db.Column(db.Float, nullable=False)
+    humidity = db.Column(db.Float, nullable=False)
+    description = db.Column(db.String(255), nullable=False)
+    generated_at = db.Column(db.DateTime, server_default=db.func.now(), nullable=False)
+    user_id = db.Column(
+        db.Integer,
+        db.ForeignKey('user.id',name='fk_weather_user_id'),
+        nullable=False
+    )
+
+    user = db.relationship('User',backref=db.backref('history', lazy=True))
+
+    def create(self):
+        db.session.add(self)
+        db.session.commit()
--- app/routes/main.py
+++ app/routes/main.py
@@ -1,11 +1,12 @@
 from flask import Blueprint,jsonify, request
 from app.services import get_current_weather, get_forecast_weather
 from flask_jwt_extended import jwt_required, get_jwt_identity
-from app.models import User
+from app.models import User, Weather
 
 bp_main = Blueprint('main',__name__)
 
 @bp_main.route('/api/v1/weather', methods=['GET'])
+@jwt_required()
 def weather():
     city = request.args.get('city')
 
@@ -15,11 +16,24 @@ def weather():
     weather_data = get_current_weather(city)
 
     if weather_data:
+        # Save the weather data to the database
+        current_user_id = get_jwt_identity()
+        main_data = {
+            'city': weather_data.get('name'),
+            'temperature': weather_data.get('main').get('temp'),
+            'feels_like': weather_data.get('main').get('feels_like'),
+            'humidity': weather_data.get('main').get('humidity'),
+            'description': weather_data.get('weather')[0].get('description'),
+            'user': User.query.filter_by(id=current_user_id).first()
+        }
+        Weather(**main_data).create()
+
         return weather_data
     else:
         return jsonify({"error": "Unable to obtain weather data"}), 404
 
 @bp_main.route('/api/v1/forecast', methods=['GET'])
+@jwt_required()
 def forecast():
     city = request.args.get('city')
     if not city:
@@ -35,12 +49,24 @@ def forecast():
 @bp_main.route('/api/v1/me',methods=['GET'])
 @jwt_required()
 def me():
+    # Page where the user can see their data and history
     current_user_id = get_jwt_identity()
     user = User.query.filter_by(id=current_user_id).first()
     user_data = {
         "id": user.id,
         "username": user.name,
-        "email": user.email
+        "email": user.email,
+        "history": [
+            {
+                "city": weather_data.city,
+                "temperature": weather_data.temperature,
+                "feels_like": weather_data.feels_like,
+                "humidity": weather_data.humidity,
+                "description": weather_data.description,
+                "generated_at": weather_data.generated_at
+            }
+            for weather_data in user.history
+        ]
     }
 
     return jsonify(user_data), 200
